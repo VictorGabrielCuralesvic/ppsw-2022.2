6e6b8156b542f55b28eceafd2cbd5796e40b1a85
Nome <miranhaomi@gmail.com>
2023-03-23 15:30:49 -0300

    Atualizando a classe SlideItem
    Atualizando a classe SlideItem, e sua principal modificação é justamente na mudança do Draw, pois agora a classe recebe um atributo do tipo Drawer da classe abstrata e o draw da SlideItem é atualizado de acordo com o método draw da Drawer. (fazer um SlideItemDrawer?)

--- src/main/java/br/upe/ppsw/jabberpoint/model/SlideItem.java
+++ src/main/java/br/upe/ppsw/jabberpoint/model/SlideItem.java
@@ -4,9 +4,12 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.ImageObserver;
 
+import br.upe.ppsw.jabberpoint.viewer.Drawer;
+
 public abstract class SlideItem {
 
   private int level = 0;
+  protected Drawer drawer;
 
   public SlideItem(int lev) {
     level = lev;
@@ -20,9 +23,10 @@ public abstract class SlideItem {
     return level;
   }
 
-  public abstract Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style style);
+  public abstract Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale);
 
-  public abstract void draw(int x, int y, float scale, Graphics g, Style style,
-      ImageObserver observer);
+  public void draw(int x, int y, float scale, Graphics g, ImageObserver observer) {
+    drawer.draw(x, y, scale, g, observer, this);
+  }
 
 }
